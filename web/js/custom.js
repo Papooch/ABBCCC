c766638abab2e73badc6fa812a0517b67ec8d326
Papooch <papoochcz@gmail.com>
2019-10-12 14:14:48 +0200

    everything works

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,57 +1,169 @@
 
-var robots = [["Karel", 0], ["Ondra", 1]];
+//var robots = [["Karel", 0], ["Ondra", 1]];
+var event_data = [];
+var robot = "";
+
+Date.prototype.toDateInputValue = (function() {
+   var local = new Date(this);
+   local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+   return local.toJSON().slice(0,10);
+});
 
 function main(){
+   d = new Date();
+   today = moment()
+   console.log(today.format('YYYY-MM-DDTHH:mm'));
+   $('#date_end').val(today.format('YYYY-MM-DDTHH:mm'));
+   $('#date_start').val(today.subtract(7, "days").format('YYYY-MM-DDTHH:mm'));
+   create_event_graph();
    //add_robots()
    //$.get("http://192.168.1.9/abbccc/test.py", function(data, status){$("#middle_pane").text(data);});
+   //load_data();
+}
+
+function load_data(){
+   $("#robot_list").empty();
+   $("#event_list").empty();
    fetch_robots();
 }
 
+function load_week(){
+   d = new Date();
+   today = moment()
+   console.log(today.format('YYYY-MM-DDTHH:mm'));
+   $('#date_end').val(today.format('YYYY-MM-DDTHH:mm'));
+   $('#date_start').val(today.subtract(7, "days").format('YYYY-MM-DDTHH:mm'));
+}
+
+function load_month(){
+   d = new Date();
+   today = moment()
+   console.log(today.format('YYYY-MM-DDTHH:mm'));
+   $('#date_end').val(today.format('YYYY-MM-DDTHH:mm'));
+   $('#date_start').val(today.subtract(30, "days").format('YYYY-MM-DDTHH:mm'));
+}
+
 function fetch_robots(){
-   $.getJSON('http://192.168.1.9/abbccc/test.py',
+   $("#robot_list").append("<img src='icons/loading.gif' style='padding-left:3em'/>");
+   //console.log('http://192.168.1.9/abbccc/get_data_from_database.py?type=robots&start='+$("#date_start").val().replace("T", "%20")+'&end='+$("#date_end").val().replace("T", "%20"));
+   $.getJSON('http://192.168.1.9/abbccc/get_data_from_database.py?type=robots&start='+$("#date_start").val().replace("T", "%20")+'&end='+$("#date_end").val().replace("T", "%20"),
    function(data){
-      add_robots(data);
-      add_events(data.robots[0])
+      console.log(data);
+      $("#robot_list").empty();
+      if(data=="No data available."){
+         $("#robot_list").text("No data from the selected time period");
+      }else{
+         add_robots(data);
+      }
+      //add_events(data.elements[0])
    })
    .fail(function(){
+      $("#robot_list").empty();
       console.log("failed to fetch robots");
-      $("#event_list").text("failed to fetch robots");
+      $("#robot_list").text("failed to fetch robots");
    });
 }
 
 function add_robots(data){
-   for(robot of data.robots){
+   for(robot of data.elements){
       add_robot(robot);
    }
 }
 
-
 function add_robot(r){
-   console.log("adding robot " + r.name);
-   console.log(r);
+   //console.log("adding robot " + r.name);
+   //console.log(r);
    el = create_robot(r.name, r.robot_id, r.uptime, r.downtime);
    $("#robot_list").append(el);
    create_uptime_chart(r);
 }
 
-function add_events(data){
-   for(event of data.events){
-      add_event(event);
-   }
-   create_event_graph();
-   prepare_event_graph_data(data.events);
+function fetch_events(robot){
+   $("#event_list").empty();
+   $("#event_list").append("<img src='icons/loading.gif' style='padding-left:500px'/>");
+   $.getJSON('http://192.168.1.9/abbccc/get_data_from_database.py?type=events&robot='+robot+'&start='+$("#date_start").val().replace("T", "%20")+'&end='+$("#date_end").val().replace("T", "%20"),
+   function(data){
+      $("#event_list").empty();
+      event_data = data;
+      downtime = event_data.elements[0].stop_start_time;
+      $("#bu-info").text("Info ("+event_data.elements[0].infos+")");
+      $("#bu-warning").text("Warning ("+event_data.elements[0].warnings+")");
+      $("#bu-error").text("Error ("+event_data.elements[0].errors+")");
+      add_events(data.elements[0]);
+   })
+   .fail(function(){
+      $("#event_list").empty();
+      console.log("failed to fetch robots");
+      $("#event_list").text("failed to fetch events");
+   });
 }
 
-function add_event(e){
-   console.log("adding event" + e.time);
-   el = create_event(e.time, e.domain, e.severity, e.user_severity, e.number, e.header, e.reason);
-   $("#event_list").append(el);
+function fetch_event_detail(id){
+   $("#event_details").empty();
+   $("#event_details").append("<img src='icons/loading.gif' style='padding-left:4em'/>");
+   $.getJSON('http://192.168.1.9/abbccc/get_data_from_database.py?type=event_details&robot='+robot+'&event='+id+'&start='+$("#date_start").val().replace("T", "%20")+'&end='+$("#date_end").val().replace("T", "%20"),
+   function(data){
+      $("#event_details").empty();      
+      $("#event_details").append(create_event_detail(data.elements[0]));
+   })
+   .fail(function(){
+      $("#event_details").empty();
+      $("#event_details").text("failed to fetch events details");
+   });
+}
+
+function add_events(data){
+   var id = 0;
+   for(event of data.events){
+      add_event(id, event);
+      id += 1;
+   }
+   update_event_graph(prepare_event_graph_data(data.events));
+}
+
+function add_event(id, e){
+   //console.log("adding event" + e.time);
+   el = create_event(id, e.event_id, e.time, e.domain, e.severity, e.user_severity, e.number, e.header, e.reason);
+   $("#event_list").prepend(el);
 }
 
 function select_robot(name){
+   robot = name;
    $("#robot_name").text("Selected robot " + name);
+   fetch_events(name);
 }
 
+function highlight_event(id){
+   g.setSelection(id);
+   $("#event_id_"+id).addClass("ev_highlight");
+   //console.log(id);
+}
+
+function unhighlight_event(id){
+   g.clearSelection();
+   $("#event_id_"+id).removeClass("ev_highlight");
+}
+
+
+var last_selected = 0;
+
 function select_event(id){
+   $("[data-event_id="+last_selected+"]").removeClass("ev_selected");
+   $("[data-event_id="+id+"]").addClass("ev_selected");
+   last_selected = id;
    $("#event_details").text("Details for event id " + id);
+   fetch_event_detail(id);
+}
+
+function scroll_to_event(id){
+      scrollbar = $("#event_list").scrollTop();
+      pos = $("#event_id_"+id).position().top;
+      height = $("#event_id_"+id).height()
+
+      $("#event_list").animate({scrollTop: scrollbar + pos - height}, 800);
+}
+
+function goto_prev_next(id){
+   select_event(id);
+   scroll_to_event($("[data-event_id="+id+"]").attr('id').replace("event_id_", ""));
 }
--- web/js/prototypes.js
+++ web/js/prototypes.js
@@ -1,6 +1,6 @@
 function create_robot(name, id, up = 1, down = 1){
-   var el_name = $("<span></span>").addClass("ro_name").text(name);
-   var el_uptime = $("<span></span>").addClass("ro_uptime").text(" up: "+up+" dn: "+down+" ("+up/(up+down)*100+"%)");
+   var el_name = $("<div></div>").addClass("ro_name").text(name);
+   var el_uptime = $("<div></div>").addClass("ro_uptime").text("Uptime: "+Math.round(up/(up+down)*1000)/10+"%");
    var el_canvas = $("<canvas></canvas>").addClass("ro_canvas").attr('id', 'ro_id_' + id);
    var el_canvas_container = $("<div></div>").addClass("ro_canvas_container").append(el_canvas);
    return  $("<div></div>")
@@ -11,15 +11,35 @@ function create_robot(name, id, up = 1, down = 1){
       .click(function(){select_robot(name)});
 }
 
-function create_event(date, domain, severity, user_severity, number, header, reason){
+function create_event(id, event_id, date, domain, severity, user_severity, number, header, reason){
    el_date = $("<div></div>").addClass("ev_date").text(date);
-   el_domain = $("<div></div>").addClass("ev_date").text("domain: " + domain);
-   el_severity = $("<div></div>").addClass("ev_severity").addClass("ev_severity_" + severity);
-   el_u_severity = $("<div></div>").addClass("ev_u_severity").text("user severity: " + user_severity);
-   el_number = $("<div></div>").addClass("ev_number").text("number: " + number);
-   el_header = $("<div></div>").addClass("ev_header").text("header: " + header);
-   el_reason = $("<div></div>").addClass("ev_reason").text("reason: " + reason);
-   return $("<div></div>").addClass("event")
+   switch (domain) {
+      case "Motion":
+         dom = "ev_domain_motion";
+         break;
+      case "Operational":
+         dom = "ev_domain_operational";
+         break;
+      case "Connected Services":
+         dom = "ev_domain_connected";
+         break;
+      case "Program":
+         dom = "ev_domain_program";
+         break;
+      case "System":
+         dom = "ev_domain_system";
+         break;
+      default:
+         dom = "ev_domain_unknown";
+         break;
+   }
+   el_domain = $("<div></div>").addClass("ev_domain").addClass(dom).text(domain);
+   el_severity = $("<div></div>").addClass("ev_severity").text(severity);
+   el_u_severity = $("<div></div>").addClass("ev_u_severity").text(user_severity);
+   el_number = $("<div></div>").addClass("ev_number").text(number);
+   el_header = $("<div></div>").addClass("ev_header").text(header);
+   el_reason = $("<div></div>").addClass("ev_reason").text(reason);
+   return $("<div></div>").addClass("event").addClass("ev_severity_" + severity)
       .append(el_severity)
       .append(el_date)
       .append(el_domain)
@@ -27,5 +47,52 @@ function create_event(date, domain, severity, user_severity, number, header, rea
       .append(el_number)
       .append(el_header)
       .append(el_reason)
-      .click(function(){select_event(Math.random())});
+      .attr('id', 'event_id_' + id)
+      .attr('data-event_id', event_id)
+      .mouseover(function(e){highlight_event(id)})
+      .mouseout(function(e){unhighlight_event(id)})
+      .click(function(){select_event(event_id)});
+}
+
+function create_event_detail(d){
+   el_sev_color = $("<div></div>").addClass("e_severity").addClass("e_severity_" + d.severity);
+
+   el_date = $("<div class='e_property e_date'><div>Date</div><div>"+d.time+"</div></div>");
+   el_domain = $("<div class='e_property e_domain'><div>Domain</div><div>"+d.domain+"</div></div>");
+   el_number = $("<div class='e_property e_number'><div>Number</div><div>"+d.number+"</div></div>");
+   el_header = $("<div class='e_property e_header'><div>Header</div><div>"+d.header+"</div></div>");
+   el_reason = $("<div class='e_property e_reason'><div>Reason</div><div>"+d.reason+"</div></div>");
+   el_cause = $("<div class='e_property e_cause'><div>Cause</div><div>"+d.cause+"</div></div>");
+   el_action = $("<div class='e_property e_action'><div>Action</div><div>"+d.action+"</div></div>");
+   el_properties = $("<div></div>").addClass("e_properties")
+      .append(el_date)
+      .append(el_domain)
+      .append(el_number);
+
+   el_long_properties = $("<div></div>").addClass("e_long_properties")
+   .append(el_header)
+   .append(el_reason)
+   .append(el_cause)
+   .append(el_action);
+
+   el_occurences = $("<div class='e_occurences'><div>Number of occurences</div><div>"+d.occurences+"</div></div>");
+   
+   el_event_stats = $("<div></div>").addClass("e_stats")
+      .append(el_occurences)
+
+   if(d.next_occurence[0]){
+      el_next = $("<div class='e_next' data-next='"+d.next_occurence[0]+"'><div>Next occurence</div><div>"+d.next_occurence[1]+"</div></div>").click(function(){goto_prev_next(d.next_occurence[0])});;
+      el_event_stats.append(el_next);
+   }
+   if(d.last_occurence[0]){
+      el_prev = $("<div class='e_prev' data-prev='"+d.last_occurence[0]+"'><div>Previous occurence</div><div>"+d.last_occurence[1]+"</div></div>").click(function(){goto_prev_next(d.last_occurence[0])});
+      el_event_stats.append(el_prev);
+   }
+
+   return $("<div></div>").attr('id', 'event_detail')
+      .append(el_sev_color)
+      .append(el_properties)
+      .append(el_long_properties)
+      .append(el_event_stats);
+      
 }
